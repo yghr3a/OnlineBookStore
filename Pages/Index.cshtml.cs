4089a5ce2c72a552d6175209611e864d586b8871
yghr3a <1764016008@qq.com>
2025-09-29 23:32:00 +0800

    # [2025/9/28] 添加文件夹和创建一些类

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineBookStore.Models.ViewModels;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace OnlineBookStore.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<BookViewModel> Books { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +17,18 @@ namespace OnlineBookStore.Pages
 
         public void OnGet()
         {
-
+            Books = new List<BookViewModel>
+            {
+                new BookViewModel { Name = "C#高级编程", CoverImageUrl = "/images/csharp.png" },
+                new BookViewModel { Name = "ASP.NET Core实战", CoverImageUrl = "/images/aspnet.png" },
+                new BookViewModel { Name = "设计模式指南", CoverImageUrl = "" },
+                new BookViewModel { Name = "C#高级编程", CoverImageUrl = "/images/csharp.png" },
+                new BookViewModel { Name = "ASP.NET Core实战", CoverImageUrl = "/images/aspnet.png" },
+                new BookViewModel { Name = "设计模式指南", CoverImageUrl = "" },
+                new BookViewModel { Name = "C#高级编程", CoverImageUrl = "/images/csharp.png" },
+                new BookViewModel { Name = "ASP.NET Core实战", CoverImageUrl = "/images/aspnet.png" },
+                new BookViewModel { Name = "设计模式指南", CoverImageUrl = "" } 
+            };
         }
     }
 }
