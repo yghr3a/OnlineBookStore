efdf0929b5b07358499fe1102f04d96c9ede039b
yghr3a <1764016008@qq.com>
2025-10-03 02:41:48 +0800

    #[2025/10/3] 初步建立了 AppDbContext -> Repository -> Service -> Index.cshtml.cs 的简单架构;
    重新安装了EFCore相关的包, 项目现在可以使用数据库迁移了

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using OnlineBookStore.Services;
 using OnlineBookStore.Repository;
 
 namespace OnlineBookStore
@@ -12,6 +13,19 @@ namespace OnlineBookStore
             // Add services to the container.
             builder.Services.AddRazorPages();
 
+            // MySQL 配置, 若开发事件充分, 可将配置信息放进appsetting.json, 便于修改
+            var connectionString = "server=localhost;port=3306;database=demo;user=root;password=Abcd753!;";
+
+            // 将AppDbContext注册成服务
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            // 注册泛型仓储服务, 注意这里注册的始开放泛型类型, 相当于注册了Repository所有的具体类型
+            builder.Services.AddScoped(typeof(Repository<>), typeof(Repository<>));
+
+            // 注册图书服务类型
+            builder.Services.AddScoped<BookService, BookService>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -22,12 +36,6 @@ namespace OnlineBookStore
                 app.UseHsts();
             }
 
-            // MySQL 配置, 若开发事件充分, 可将配置信息放进appsetting.json, 便于修改
-            var connectionString = "server=localhost;port=3306;database=demo;user=root;password=Abcd753!;";
-
-            // 将AppDbContext注册成服务
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
