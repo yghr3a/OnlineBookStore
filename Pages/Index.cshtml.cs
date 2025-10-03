485d13a865c4a0ddd937c9bf25613d3b322ea329
yghr3a <1764016008@qq.com>
2025-10-04 03:50:06 +0800

    # [2025/10/4] 页面切换功能实装
    ## 添加Add
    - 主页面 Index.cshtml.cs 添加了pageIndex和pageSize作为url参数, 来访问所需要的内容
    - 主页面添加了切换上下页的按钮控件
    
    ## 重构Refatory
    - 主页面 Index.cshtml.cs 使用 BookService.GetPoplurBookAsync() 方法分页获取热销书籍视图模型

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,6 +13,13 @@ namespace OnlineBookStore.Pages
 
         public List<BookViewModel> Books { get; set; } = new List<BookViewModel>();
 
+        // 当前页码，从 URL 上获取
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+
+        // 每页多少条数据，可以写死，也可以通过 URL 传递
+        public int PageSize { get; set; } = 50;
+
         public IndexModel(ILogger<IndexModel> logger, BookService bookService)
         {
             _logger = logger;
@@ -20,10 +27,11 @@ namespace OnlineBookStore.Pages
         }
 
         // 业务方法不能使用 async void 类型函数
-        // [2025/10/3] 改为使用async Task类型
+        // [2025/10/3] 改为使用async Task类型, async void不推荐使用, 除非是事件处理器
+        // [2025/10/4] 增加分页参数
         public async Task OnGet()
         {
-            Books = await _bookService.GetPopularBooksAsync();
+            Books = await _bookService.GetPopularBooksAsync(PageIndex, PageSize);
         }
     }
 }
