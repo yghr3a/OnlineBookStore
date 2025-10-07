20367f2f13753058145bd307462fb59e9d2a9bc3
yghr3a <1764016008@qq.com>
2025-10-08 01:20:38 +0800

    # [2025/10/8]  初步创建了 Cart 和CartItem 实体模型类

---
+++ Models/Entities/Cart.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineBookStore.Models.Entities
+{
+    public class Cart : IEntityModel
+    {
+        // 购物车项Id
+        public int id { get; set; }
+
+        // 关联的用户Id
+        public required int UserId { get; set; }
+
+        // 购物车中的购物车项
+        public required List<CartItem> CartItems { get; set; }
+
+    }
+}
