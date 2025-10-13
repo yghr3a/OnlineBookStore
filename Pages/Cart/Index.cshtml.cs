d4539f4b88e90baa0e2584cb68eb77f81e96c41f
yghr3a <1764016008@qq.com>
2025-10-13 11:50:38 +0800

    # [2025/10/13]  初步完成购物车页面模型的功能

--- Pages/Cart/Index.cshtml.cs
+++ Pages/Cart/Index.cshtml.cs
@@ -1,15 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineBookStore.Models.ViewModels;
+using OnlineBookStore.Services;
 
 namespace OnlineBookStore.Pages.Cart
 {
     public class IndexModel : PageModel
     {
+        private CartService _cartService;
 
+        public CartViewModel cartViewModel { get; set; }
+        public string ErrorMsg { get; set; }
 
-        public void OnGet()
+        public IndexModel(CartService cartService)
         {
+            _cartService = cartService;
+        }
 
+        public async Task OnGet()
+        {
+            var cartResult = await _cartService.GetUserCartAsync();
+
+            if(cartResult.IsSuccess == true)
+            {
+                cartViewModel = cartResult.CartViewModel!;
+            }
+            else
+            {
+                ErrorMsg = cartResult.ErrorMsg;
+            }
         }
     }
 }
