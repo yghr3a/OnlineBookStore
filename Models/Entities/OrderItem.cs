81d584cea7b3745bb4e6a70a5a90da4fae534b3c
yghr3a <1764016008@qq.com>
2025-10-14 14:46:09 +0800

    # [2025/10/14] 初步创建了Order 和 OrderItem 两个的实体模型类

---
+++ Models/Entities/OrderItem.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineBookStore.Models.Entities
+{
+    /// <summary>
+    /// 订单项
+    /// </summary>
+    public class OrderItem : IEntityModel
+    {
+        // 订单项Id
+        public int Id { get; set; }
+
+        // 所隶属的Order的Id
+        [ForeignKey("Order")]
+        public int OrderId {  get; set; }
+        // 导航属性
+        public Order? Order { get; set; }
+
+        // 书籍Id
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+        // 导航属性
+        public Book? Book { get; set; }
+
+        // 购买时的单价
+        public decimal? Price { get; set; }
+        // 购买数量
+        public int Count {  get; set; }
+    }
+}
