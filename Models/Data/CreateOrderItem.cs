e5256109a70d0b2fa43a288054f151f2e4467e37
yghr3a <1764016008@qq.com>
2025-10-15 15:31:33 +0800

    # [2025/10/14] 添加了CreateOrderResponse 和 CreateOrderItem 两个作为创建订单业务方法的参数类型

---
+++ Models/Data/CreateOrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineBookStore.Models.Data
+{
+    /// <summary>
+    /// 创建订单请求类型单项
+    /// </summary>
+    public class CreateOrderItem
+    {
+        // 书籍编号
+        public int BookNumber { get; set; }
+        // 购买数量
+        public int Count { get; set; }
+    }
+}
---
+++ Models/Data/CreateOrderResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineBookStore.Models.Data
+{
+    /// <summary>
+    /// 创建订单请求类型, 作为创建订单的参数类型
+    /// </summary>
+    public class CreateOrderResponse
+    {
+        public List<CreateOrderItem> Items { get; set; } = new();
+    }
+}
