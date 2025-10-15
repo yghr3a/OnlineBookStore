327f1e11fb9c186d048fcda9ea1a7fcf7e0780f6
yghr3a <1764016008@qq.com>
2025-10-15 15:31:33 +0800

    # [2025/10/14] 添加了CreateOrderResponse 和 CreateOrderItem 两个作为创建订单业务方法的参数类型

---
+++ Services/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.DataProtection.Repositories;
+using OnlineBookStore.Models.Entities;
+using OnlineBookStore.Respository;
+
+namespace OnlineBookStore.Services
+{
+    /// <summary>
+    /// 负责订单相关的业务操作
+    /// </summary>
+    public class OrderService
+    {
+        private Respository<Book> _bookRepository;
+        private Respository<Order> _orderRepository;
+        private Respository<Cart> _cartRespository;
+        private UserContext _userContext;
+
+        public OrderService(UserContext userContext,
+                            Respository<Book> bookRepository,
+                            Respository<Order> orderRepository,
+                            Respository<Cart> cartRespository) 
+        {
+            _userContext = userContext;
+            _bookRepository = bookRepository;
+            _orderRepository = orderRepository;
+            _cartRespository = cartRespository;
+        }
+
+        public async Task CreatOrder()
+        {
+
+        }
+    }
+}
