f7d61dd651c49885d33cda964fcab821e9b44e90
yghr3a <1764016008@qq.com>
2025-10-16 13:47:25 +0800

    # [2025/10/16] 优化下单业务流程方法, 采用DDD架构思维对部分服务类进行优化和解耦
    ## 添加Add
    - InfoResult类, 不同于ServiceResult, 仅仅传递操作是否成功与错误码
    - OrderDomainService 增加了添加订单的单项业务方法

---
+++ Services/BookDomainService.cs
@@ -0,0 +1,59 @@
+﻿using OnlineBookStore.Models.Data;
+using OnlineBookStore.Models.Entities;
+using OnlineBookStore.Respository;
+
+namespace OnlineBookStore.Services
+{
+    /// <summary>
+    /// 书籍领域模型
+    /// </summary>
+    public class BookDomainService
+    {
+        private Respository<Book> _bookRespository;
+
+        public BookDomainService(Respository<Book> bookRespository) 
+        {
+            _bookRespository = bookRespository;
+        }
+
+        /// <summary>
+        /// 根据书籍编号列表获取图书实体模型列表
+        /// [2025/10/16] 职责调整, 转交给领域服务负责
+        /// </summary>
+        /// <param name="BookNumbers"></param>
+        /// <returns></returns>
+        public async Task<ServiceResult<List<Book>>> GetBookByNumberAsync(List<int> BookNumbers)
+        {
+            var booksQurey = _bookRespository.AsQueryable().Where(b => BookNumbers.Contains(b.Number));
+            var books = await _bookRespository.GetListByQueryAsync(booksQurey);
+            var ErrorMsg = string.Empty;
+
+            foreach (var n in BookNumbers)
+                if (books.Find(b => b.Number == n) is null)
+                    ErrorMsg += $"编号{n}:书籍不存在\n";
+
+            if (ErrorMsg != string.Empty)
+                return ServiceResult<List<Book>>.Fail(ErrorMsg.Trim());
+
+            return ServiceResult<List<Book>>.Success(books);
+        }
+
+        /// <summary>
+        /// 根据图书编号获取单个图书实体模型
+        /// [2025/10/16] 职责调整, 转交给领域服务负责
+        /// </summary>
+        /// <param name="BookNumbers"></param>
+        /// <returns></returns>
+        public async Task<ServiceResult<Book>> GetBookByNumberAsync(int BookNumber)
+        {
+            var booksQurey = _bookRespository.AsQueryable().Where(b => b.Number == BookNumber);
+            var book = await _bookRespository.GetSingleByQueryAsync(booksQurey);
+
+            if (book is null)
+                return ServiceResult<Book>.Fail($"编号{BookNumber}:书籍不存在");
+
+            return ServiceResult<Book>.Success(book);
+        }
+
+    }
+}
--- Services/OrderDomainService.cs
+++ Services/OrderDomainService.cs
@@ -1,4 +1,8 @@
 ﻿using OnlineBookStore.Respository;
+using OnlineBookStore.Models;
+using OnlineBookStore.Models.Data;
+using OnlineBookStore.Models.Entities;
+using Microsoft.AspNetCore.Routing.Matching;
 
 namespace OnlineBookStore.Services
 {
@@ -8,8 +12,49 @@ namespace OnlineBookStore.Services
     public class OrderDomainService
     {
         private UnitOfWork _unitOfWork;
+        private Respository<Book> _bookRespository;
+        private Respository<Order> _orderRespository;
 
-        public OrderDomainService(UnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        public OrderDomainService(UnitOfWork unitOfWork, 
+                                  Respository<Book> bookRespository, 
+                                  Respository<Order> orderRespository)
+        {
+            _unitOfWork = unitOfWork;
+            _bookRespository = bookRespository;
+            _orderRespository= orderRespository;
+        }
         
+        /// <summary>
+        /// 添加订单, 更新书籍销量, 更新用户历史订单
+        /// [2025/10/16] 错误情况处理先简单实现, 后续使用业务异常来处理更好
+        /// </summary>  
+        public async Task<InfoResult> AddOrder(User user, Order order, List<Book> books, CreateOrderResponse response)
+        {
+            if (user is null) return InfoResult.Fail("用户信息为空");
+            if (order is null) return InfoResult.Fail("订单信息为空");
+            if (books is null) return InfoResult.Fail("书籍信息为空");
+            if (books.Count != response.Items.Count) return InfoResult.Fail("部分书籍不存在");
+
+            // 建立字典
+            var BookNumber2Count = response.Items.ToDictionary(i => i.BookNumber, i => i.Count);
+
+            await _unitOfWork.ExecuteInTransactionAsync( async () =>
+            {
+                // 添加订单到数据库里
+                await _orderRespository.AddAsync(order);
+
+                // 更新各个书籍的销量
+                foreach (var book in books)
+                {
+                    book.Sales += BookNumber2Count[book.Number];
+                    _bookRespository.Update(book);
+                }
+
+                // 更新用户历史订单
+                user.Orders.Add(order);
+            });
+
+            return InfoResult.Success();
+        }
     }
 }
