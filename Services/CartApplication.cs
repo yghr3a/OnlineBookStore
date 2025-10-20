1441757c201d0b29c8fbd024c6008e59c4b588fa
yghr3a <1764016008@qq.com>
2025-10-20 20:19:18 +0800

    # [2025/10/20] 业务代码优化 ## 添加Add - CartDomainService添加了 AddBookToCartAsync 方法, 负责执行将参数中的book对象
    ## 重构Refactory
    - 重构了 CartApplication 的 AddBookToUserCart 方法, 协调各个领域服务

--- Services/CartApplication.cs
+++ Services/CartApplication.cs
@@ -11,7 +11,8 @@ namespace OnlineBookStore.Services
         // 用户上下文信息
         private UserContext _userContext;
         private CartDomainService _cartDomainService;
-        private UserDomainService _accountService;
+        private UserDomainService _userDomainService;
+        private BookDomainService _bookDomainService;
         private Respository<User> _userRespository;
         private Respository<Book> _bookRespository;
         private Respository<Cart> _cartRespository;
@@ -19,14 +20,16 @@ namespace OnlineBookStore.Services
         // 在犹豫是否有必要定义一个User属性和Cart属性,直接通过UserContext获取用户Id, 然后通过AppDbContext获取用户和购物车对象似乎也挺方便的
         public CartApplication(UserContext userContext,
                            CartDomainService cartDomainService,
-                           UserDomainService accountService,
+                           UserDomainService userDomainService,
+                           BookDomainService bookDomainService,
                            Respository<User> userRespository,
                            Respository<Book> bookRespository,
                            Respository<Cart> cartRespository)
         {
             _userContext = userContext;
             _cartDomainService = cartDomainService;
-            _accountService = accountService;
+            _userDomainService = userDomainService;
+            _bookDomainService = bookDomainService;
 
             _userRespository = userRespository;
             _bookRespository = bookRespository;
@@ -35,82 +38,31 @@ namespace OnlineBookStore.Services
 
         /// <summary>
         /// 将书籍添加到当前登录用户的购物车
+        /// [2025/10/20] 业务优化, 获取用户的工作交给UserDomainService, 获取购物车的工作交给CartDomainService, 获取书籍的工作交给BookDomainService
         /// </summary>
         /// <param name="bookNumber"></param>
         /// <returns></returns>
-        public async Task<CartAddResult> AddBookToUserCart(int bookNumber)
+        public async Task<InfoResult> AddBookToUserCart(int bookNumber)
         {
-            var userQuary = _userRespository.AsQueryable();
-            var bookQuary = _bookRespository.AsQueryable();
+            var userRes = await _userDomainService.GetCurrentUserEntityModelAsync();
+            if (userRes.IsSuccess == false)
+                return InfoResult.Fail(userRes!.ErrorMsg);
+            var user = userRes.Data;
 
-            // TODO :发现UserContext里的UserId(用户编号)是string类型, 但是User实体类里的Number(用户编号)是int类型
+            var bookRes = await _bookDomainService.GetBookByNumberAsync(bookNumber);
+            if (bookRes.IsSuccess == false)
+                return InfoResult.Fail(bookRes!.ErrorMsg);
+            var book = bookRes.Data;
 
-            // 这里使用用户名作为索引 
-            // [2025/10/11] 要使用Include和ThenInclude来加载相关的Cart和CartItems
-            var user = await userQuary.Include(u => u.Cart)
-                                      .ThenInclude(c => c.CartItems)
-                                      .FirstOrDefaultAsync(u => u.UserName == _userContext.UserName);
+            var cartRes = await _cartDomainService.GeCartByUserIdAsync(user!.Id);
+            if (cartRes.IsSuccess == false)
+                return InfoResult.Fail(cartRes.ErrorMsg);
+            var cart = cartRes.Data;
 
-            var book = await bookQuary.FirstOrDefaultAsync(b => b.Number == bookNumber);
-            var cart = user?.Cart;
+            // 调用领域服务的方法将书籍添加到购物车
+            var addRes = await _cartDomainService.AddBookToCartAsync(book!, cart!);
 
-            if (user == null)
-            {
-                // 用户不存在, 可以抛出异常或返回错误信息
-                return new CartAddResult()
-                {
-                    IsSuccess = false,
-                    ErrorMsg = "用户不存在"
-                };
-            }
-
-            if (book == null)
-            {
-                // 书籍不存在, 可以抛出异常或返回错误信息
-                return new CartAddResult()
-                {
-                    IsSuccess = false,
-                    ErrorMsg = "书籍不存在"
-                };
-            }
-
-            if (cart == null)
-            {
-                // 如果用户没有购物车, 创建一个新的购物车
-                // Cart与User一一对应, 无需手动给Cart.UserId赋值, EFCore会自动处理
-                cart = new Cart();
-                user.Cart = cart;
-
-                // 保存数据, 因为此时cart里的UserID还没有值, 需要先保存User和Cart, 让cart.UserID有值
-                await _cartRespository.SaveAsync();
-                await _userRespository.SaveAsync();
-            }
-
-            var cartItem = new CartItem()
-            {
-                CartId = cart.UserId,
-                BookId = book.Id,
-                Count = 1,                 // 数量先设为一, 后续可以扩展为传入参数
-                CreatedDate = DateTime.Now // 设置添加时间, 这里先简单处理
-            };
-
-            // 将书籍添加到购物车
-            if (cart.CartItems == null)
-            {
-                cart.CartItems = new List<CartItem>();
-            }
-
-            // [2025/10/17] 出现了一个bug, 重复添加同一本书籍时会出现多条"同种书籍, 购买数为一"的记录, 需要先检查是否已经存在该书籍的CartItem
-            var exitItem = cart.CartItems.Find(i => i.BookId == book.Id);
-            if (exitItem is null)
-                cart.CartItems?.Add(cartItem);  // 若不存在则添加新的CartItem
-            else
-                exitItem.Count += 1; // 已存在则数量加一
-
-            // 保存数据, EFCore会自动处理关联关系
-            await _cartRespository.SaveAsync();
-
-            return new CartAddResult() { IsSuccess = true };
+            return InfoResult.Success();
         }
 
         /// <summary>
@@ -174,7 +126,7 @@ namespace OnlineBookStore.Services
         public async Task<InfoResult> RemoveUserCartSingleItemAsync(int orderItemNumber)
         {
             // 获取当前用户实体模型
-            var userRes = await _accountService.GetCurrentUserEntityModelAsync();
+            var userRes = await _userDomainService.GetCurrentUserEntityModelAsync();
             if (userRes.IsSuccess == false)
                 return InfoResult.Fail(userRes.ErrorMsg);
             var user = userRes.Data;
@@ -208,7 +160,7 @@ namespace OnlineBookStore.Services
         public async Task<InfoResult> ClearUserCartAsync()
         {
             // 获取当前用户实体模型
-            var userRes = await _accountService.GetCurrentUserEntityModelAsync();
+            var userRes = await _userDomainService.GetCurrentUserEntityModelAsync();
             if (userRes.IsSuccess == false)
                 return InfoResult.Fail(userRes.ErrorMsg);
             var user = userRes.Data;
